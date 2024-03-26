2761820e73685f25b93a9c9aa98ce7fa5584f670
apehex <apehex@protonmail.com>
2024-03-26 22:09:38 +0100

    Throttle scraping to avoid bans

--- feedblocks/scrape.py
+++ feedblocks/scrape.py
@@ -1,52 +1,42 @@
+import functools
 import json
+import logging
 import os
+import time
 
-import pyarrow
 import pyarrow.lib as pl
-import pyarrow.parquet as pq
 import requests
-import web3
 
-# SCHEMA ######################################################################
-
-BASE_SCHEMA = pyarrow.schema(fields=[
-    pyarrow.lib.field('chain_id', pyarrow.uint64()),
-    pyarrow.lib.field('block_number', pyarrow.uint32()),
-    pyarrow.lib.field('block_hash', pyarrow.binary()),
-    pyarrow.lib.field('transaction_hash', pyarrow.binary()),
-    pyarrow.lib.field('deployer', pyarrow.binary()),
-    pyarrow.lib.field('factory', pyarrow.binary()),
-    pyarrow.lib.field('contract_address', pyarrow.binary()),
-    pyarrow.lib.field('create_index', pyarrow.uint32()),
-    pyarrow.lib.field('init_code', pyarrow.binary()),
-    pyarrow.lib.field('init_code_hash', pyarrow.binary()),
-    pyarrow.lib.field('code', pyarrow.binary()),
-    pyarrow.lib.field('code_hash', pyarrow.binary()),
-    pyarrow.lib.field('n_init_code_bytes', pyarrow.uint32()),
-    pyarrow.lib.field('n_code_bytes', pyarrow.uint32()),])
-
-EXTRA_SCHEMA = pyarrow.schema(fields=[
-    pyarrow.lib.field('source_code', pyarrow.binary()),])
-
-SCHEMA = pyarrow.unify_schemas(schemas=[BASE_SCHEMA, EXTRA_SCHEMA])
-
-# DATASET #####################################################################
-
-
-
-# TARGETS #####################################################################
-
-# SOURCE CODE #################################################################
+# META ########################################################################
 
 ETH_API_KEY = os.environ.get('ETH_API_KEY', '')
 ETH_API_URL = 'https://api.etherscan.io/api?module=contract&action=getsourcecode&address={address}&apikey={key}'
+FREQUENCY = 9. # Hz / calls per second
 
-__a = '0xc62bdA6F3f7158Bc263Ebc188155F398c6167cE2'
+# RATE LIMIT ##################################################################
 
-__d = pq.ParquetDataset('data/', schema=SCHEMA)
-__t = __d.read()
-__c = pyarrow.compute.field('contract_address') == bytes.fromhex('001BD7EF3B7424A18DA412C7AAA3B6534BA7816E')
-__t.filter(__c)
+def pace(freq: float) -> callable:
+    """Creates a decorator thats throttles a function to a specified frequency."""
+    __ms = 1. / freq
+    __prev = 0.
+
+    def __decorator(func: callable) -> callable:
+        @functools.wraps(func)
+        def __wrapper(*args, **kwargs):
+            """Space calls to this functions to satisfy a rate limit."""
+            nonlocal __prev
+            __now = time.time() / 1000.
+            # sleep until next time slot
+            time.sleep(max(0., __prev + __ms - __now))
+            # update the time of the previous call
+            __prev = __now
+            # actually output the original function's result
+            return func(*args, **kwargs)
+        return __wrapper
+
+    return __decorator
+
+# SOURCE CODE #################################################################
 
 def get_source(address: str, key: str=ETH_API_KEY) -> bytes:
     __b = b''
@@ -56,23 +46,47 @@ def get_source(address: str, key: str=ETH_API_KEY) -> bytes:
         __j = json.loads(__r.text)
         __b = __j['result'][0]['SourceCode'].encode('utf-8')
     except Exception:
-        pass
+        return None
     return __b
 
-def scrape_source(table: pl.Table, schema: pl.Schema=SCHEMA, key: str=ETH_API_KEY) -> pl.Table:
-    __i = 0
+def scrape_source(table: pl.Table, key: str=ETH_API_KEY, freq: float=FREQUENCY) -> pl.Table:
+    # global stats
+    __batch_count = 0
+    # output
     __table = []
+    # rate limited get
+    __get = pace(freq=freq)(get_source) # rate limit on API queries
     for __batch in table.to_batches(max_chunksize=128):
+        # split
         __rows = __batch.to_pylist()
+        # batch stats
+        logging.info('batch {}...'.format(__batch_count))
+        __error_count = 0
+        __skipped_count = 0
+        __not_found_count = 0
+        __ok_count = 0
         for __r in __rows:
             if __r and __r.get('contract_address', b'') and not __r.get('source_code', b''):
-                __r['source_code'] = get_source(address=__r.get('contract_address', b''))
+                # query API
+                __a = '0x' + __r.get('contract_address', b'').hex()
+                __r['source_code'] = __get(address=__a, key=key)
+                # stats
+                if __r['source_code'] is None:
+                    __error_count +=1
+                elif not __r['source_code']:
+                    __not_found_count += 1
+                else:
+                    __ok_count +=1
+            else:
+                __skipped_count +=1
+            # save data
             __table.append(__r)
-    return pl.Table.from_pylist(mapping=__table, schema=schema, key=key)
+        # log
+        logging.info('stats:\nsuccessful: {ok}\nnot found: {found}\nskipped: {skipped}\nerrors: {errors}\n'.format(ok=__ok_count, found=__not_found_count, skipped=__skipped_count, errors=__error_count))
+        __batch_count += 1
+    return pl.Table.from_pylist(mapping=__table, schema=table.schema)
 
 # RUNTIME BYTECODE ############################################################
-
-# pq.write_to_dataset(__t, root_path='data/')
 
 # iterate over dataset row by row
 
