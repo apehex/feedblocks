cf710ea0df532efeffbf6a8845d797e2c33daf6e
apehex <apehex@protonmail.com>
2024-03-27 17:23:31 +0100

    Tidy cryo datasets

--- feedblocks/data.py
+++ feedblocks/data.py
@@ -1,37 +1,38 @@
+import os
+
 import pyarrow
 import pyarrow.lib as pl
 import pyarrow.parquet as pq
 
 # SCHEMA ######################################################################
 
-BASE_SCHEMA = pyarrow.schema(fields=[
-    pyarrow.lib.field('chain_id', pyarrow.uint64()),
-    pyarrow.lib.field('block_number', pyarrow.uint32()),
-    pyarrow.lib.field('block_hash', pyarrow.binary()),
-    pyarrow.lib.field('transaction_hash', pyarrow.binary()),
-    pyarrow.lib.field('deployer', pyarrow.binary()),
-    pyarrow.lib.field('factory', pyarrow.binary()),
-    pyarrow.lib.field('contract_address', pyarrow.binary()),
-    pyarrow.lib.field('create_index', pyarrow.uint32()),
-    pyarrow.lib.field('init_code', pyarrow.binary()),
-    pyarrow.lib.field('init_code_hash', pyarrow.binary()),
-    pyarrow.lib.field('code', pyarrow.binary()),
-    pyarrow.lib.field('code_hash', pyarrow.binary()),
-    pyarrow.lib.field('n_init_code_bytes', pyarrow.uint32()),
-    pyarrow.lib.field('n_code_bytes', pyarrow.uint32()),])
-
-EXTRA_SCHEMA = pyarrow.schema(fields=[
-    pyarrow.lib.field('source_code', pyarrow.binary()),])
-
-SCHEMA = pyarrow.unify_schemas(schemas=[BASE_SCHEMA, EXTRA_SCHEMA])
+SCHEMAS ={
+    'contracts': pyarrow.schema(fields=[
+        pl.field('chain_id', pyarrow.uint64()),
+        pl.field('block_number', pyarrow.uint32()),
+        pl.field('block_hash', pyarrow.binary()),
+        pl.field('transaction_hash', pyarrow.binary()),
+        pl.field('deployer', pyarrow.binary()),
+        pl.field('factory', pyarrow.binary()),
+        pl.field('contract_address', pyarrow.binary()),
+        pl.field('create_index', pyarrow.uint32()),
+        pl.field('init_code', pyarrow.binary()),
+        pl.field('init_code_hash', pyarrow.binary()),
+        pl.field('code', pyarrow.binary()),
+        pl.field('code_hash', pyarrow.binary()),
+        pl.field('n_init_code_bytes', pyarrow.uint32()),
+        pl.field('n_code_bytes', pyarrow.uint32()),
+        pl.field('source_code', pyarrow.binary()),]),}
 
 # DATASET #####################################################################
 
-__a = '0xc62bdA6F3f7158Bc263Ebc188155F398c6167cE2'
+DATA_PATH = 'data/{chain}/{dataset}/'
 
-__d = pq.ParquetDataset('data/', schema=SCHEMA)
-__t = __d.read()
-__c = pyarrow.compute.field('contract_address') == bytes.fromhex('001BD7EF3B7424A18DA412C7AAA3B6534BA7816E')
-__t.filter(__c)
+def tidy(path: str='data') -> None:
+    __files = [__p.split('__') for __p in os.listdir(path) if os.path.isfile(os.path.join(path, __p))]
+    for __f in __files:
+        __old = os.path.join(path, '__'.join(__f))
+        __new = os.path.join(path, *__f)
+        os.rename(__old, __new)
 
-# pq.write_to_dataset(__t, root_path='data/')
+# __c = pyarrow.compute.field('contract_address') == bytes.fromhex('001BD7EF3B7424A18DA412C7AAA3B6534BA7816E')
