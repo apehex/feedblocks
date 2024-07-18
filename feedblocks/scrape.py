0319113b111b3cd4d7e38ea5ef5b75c8661e1c99
apehex <apehex@protonmail.com>
2024-07-18 21:07:55 +0200

    Add retry to the scraping fn

--- feedblocks/scrape.py
+++ feedblocks/scrape.py
@@ -16,9 +16,9 @@ RATE_LIMIT_INFURA = 10. # Hz / calls per second
 
 # RATE LIMIT ##################################################################
 
-def pace(freq: float) -> callable:
+def pace(freq: float, backoff: float=1.0, limit: int=4) -> callable:
     """Creates a decorator thats throttles a function to a specified frequency."""
-    __ms = 1. / freq
+    __wait = 1. / freq
     __prev = 0.
 
     def __decorator(func: callable) -> callable:
@@ -26,13 +26,30 @@ def pace(freq: float) -> callable:
         def __wrapper(*args, **kwargs):
             """Space calls to this functions to satisfy a rate limit."""
             nonlocal __prev
-            __now = time.time() / 1000.
-            # sleep until next time slot
-            time.sleep(max(0., __prev + __ms - __now))
-            # update the time of the previous call
-            __prev = __now
-            # actually output the original function's result
-            return func(*args, **kwargs)
+            # init
+            __attempts = 0
+            __result = None
+            # attempt until success or max retries is reach
+            while __attempts < max(1, limit):
+                # sleep until next time slot
+                time.sleep(max(0., __prev + __wait - 0.001 * time.time()))
+                # ready
+                try:
+                    # actually call the original function
+                    __result = func(*args, **kwargs)
+                    # play it safe and take the time of return as ref for the next call
+                    __prev = 0.001 * time.time()
+                    # get out of the retry loop
+                    return __result
+                except Exception as __e:
+                    __attempts += 1
+                    # increase waiting time
+                    __prev += __attempts * backoff
+                    # log args + error
+                    logging.warning(f'attempt {__attempts} failed: f({args} + {kwargs}) => error "{str(__e)}"')
+            # run out of attempts
+            return None
+        # return the wrapped function
         return __wrapper
 
     return __decorator
@@ -40,15 +57,8 @@ def pace(freq: float) -> callable:
 # SOURCE CODE #################################################################
 
 def get_source(address: str, url: str=ETH_API_URL, key: str=ETH_API_KEY) -> bytes:
-    __b = b''
-    try:
-        # query etherscan
-        __r = requests.get(url.format(address=address, key=key))
-        __j = json.loads(__r.text)
-        __b = __j['result'][0]['SourceCode'].encode('utf-8')
-    except Exception as __e:
-        logging.debug(str(__e))
-        return None
-    return __b
+    __r = requests.get(url.format(address=address, key=key))
+    __j = json.loads(__r.text)
+    return __j['result'][0]['SourceCode'].encode('utf-8')
 
-get_source_from_etherscan = pace(freq=RATE_LIMIT_ETHERSCAN)(functools.partial(get_source, url=ETH_API_URL, key=ETH_API_KEY))
+get_source_from_etherscan = pace(freq=3.)(functools.partial(get_source, url=ETH_API_URL, key=ETH_API_KEY))
